ad47081d804ca67fecaff6771cb750b3e6cd6f41
Didac Esteban <>
2018-10-15 15:41:58 +0200

    First version of presentation

--- src/App.js
+++ src/App.js
@@ -49,11 +49,56 @@ class App extends Component {
           <p className="title">
             ¿Cómo se usa HTML?
           </p>
-          <code>
+          <div>
               &lt;b&gt; <b>negrita</b> &lt;/b&gt;
               &lt;u&gt; <u>subrayado</u> &lt;/u&gt;
               &lt;i&gt; <i>cursiva</i> &lt;/i&gt;
-          </code>
+          </div>
+        </div>) : ''}
+
+        {this.state.step === 3 ? (<div className="Content">
+          <p className="title">
+            ¿Por qué esto está mal?
+          </p>
+          <div>
+              &lt;u&gt; &lt;b&gt; negrita y subrayado &lt;/u&gt; &lt;/b&gt;
+          </div>
+        </div>) : ''}
+
+        {this.state.step === 4 ? (<div className="Content">
+          <p className="title">
+            Jerarquía
+          </p>
+          <div>
+              &lt;u&gt; &lt;b&gt; <u><b> negrita y subrayado </b></u>  &lt;/b&gt; &lt;/u&gt;
+          </div>
+        </div>) : ''}
+
+        {this.state.step === 5 ? (<div className="Content">
+          <p className="title">
+            Caracteres y símbolos especiales
+          </p>
+          <div>
+            &copy&#59; es decir &copy;
+          </div>
+        </div>) : ''}
+
+        {this.state.step === 6 ? (<div className="Content">
+          <p className="title">
+            Emojis
+          </p>
+          <div>
+              🤗 🍻
+          </div>
+        </div>) : ''}
+
+        {this.state.step === 7 ? (<div className="Content">
+          <p className="title">
+            Siguiente paso 
+          </p>
+          <div>
+              https://thetool.io/2017/how-to-use-html-emoji-googleplay
+          </div>
         </div>) : ''}
 
       </div>
